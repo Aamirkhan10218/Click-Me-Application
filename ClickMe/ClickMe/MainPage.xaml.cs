3eab8b940d178adabaee1cfc8670dc744ca2ac54
Aamir Khan <Aamirimagin@outlook.com>
2019-07-28 07:35:22 +0500

    Add project files.

---
+++ ClickMe/ClickMe/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ClickMe
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            lblValue.Text = string.Format("values id{0}", e.NewValue);
+        }
+    }
+}
